2a78af15e14cf6749d2d38b30906b7051bc2e7bc
moli73 <limo9373@gmail.com>
2017-07-09 20:18:13 -0500

    2 backtracking

--- backtracking/425.java
+++ backtracking/425.java
@@ -36,3 +36,82 @@ public class Solution {
         return true;
     }
 }
+
+//version 2: add Trie tree but to scan all trie tree leaves cost too much also TLE
+public class Solution {
+    public List<List<String>> wordSquares(String[] words) {
+        List<List<String>> res = new ArrayList<>();
+        TrieNode root = makeTrie(words);
+        helper(res, new ArrayList<String>(), new String(), root);
+        return res;
+    }
+
+    public void helper(List<List<String>> res, List<String> comb, String prefix, TrieNode root) {
+        if(comb.size() != 0 && comb.size() == comb.get(0).length()) {
+            res.add(new ArrayList<String>(comb));
+            return;
+        }
+
+        TrieNode ws = root;
+        for(int i = 0; i < prefix.length(); i++) {
+            if(ws.children[prefix.charAt(i) - 'a'] == null) {
+                return;
+            } else {
+                ws = ws.children[prefix.charAt(i) - 'a'];
+            }
+        }
+
+        List<String> next = getWords(ws);
+
+        for(String word : next) {
+            comb.add(word);
+
+            String temp = new String();
+            if(comb.size() != comb.get(0).length()) {
+                for(int i = 0; i < comb.size(); i ++) {
+                    temp += comb.get(i).charAt(comb.size());
+                }
+            }
+
+            helper(res, comb, temp, root);
+            comb.remove(comb.size() - 1);
+        }
+    }
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word = null;
+    }
+
+    public TrieNode makeTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for(String word : words) {
+            TrieNode ws = root;
+            for(int i = 0; i < word.length(); i++) {
+                if(ws.children[word.charAt(i) - 'a'] == null) {
+                    ws.children[word.charAt(i) - 'a'] = new TrieNode();
+                }
+                ws = ws.children[word.charAt(i) - 'a'];
+            }
+            ws.word = word;
+        }
+        return root;
+    }
+
+    public List<String> getWords(TrieNode root) {
+        List<String> res = new ArrayList<String>();
+        if(root == null) {
+            return res;
+        }
+        if(root.word != null) {
+            res.add(root.word);
+            return res;
+        }
+
+        for(int i = 0; i < 26; i++){
+            res.addAll(getWords(root.children[i]));
+        }
+
+        return res;
+    }
+}
